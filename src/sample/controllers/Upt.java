3862ab13ce0472fb97b53cd240bf5b13f942aa61
User <golikov.d.s@yandex.ru>
2017-12-15 14:27:50 +0300

    15/12/17

--- src/sample/controllers/Upt.java
+++ src/sample/controllers/Upt.java
@@ -5,8 +5,10 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
@@ -21,7 +23,6 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
     public CheckBox error_power;
     public CheckBox error_akb;
     public CheckBox upt_is_blocked;
-    public CheckBox door_open;
     public CheckBox upt_fire1;
     public CheckBox upt_fire2;
     public CheckBox error;
@@ -42,12 +43,23 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
     public TextField сondown;
     public TextField u_power;
     public TextField u_power_akb;
+    public CheckBox stop_startup;
+    public Button buttonReset;
+    public CheckBox otv_derived;
+    public CheckBox fire_from2;
+    public CheckBox fire_from1;
+    public CheckBox startup_is_goin;
+    public CheckBox door_is_open;
     private byte number;
     private byte[] state;
+    private boolean reset;
+
+    private int startupCondown;
 
     public Upt() {
     }
 
+    int counterCondown;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -67,6 +79,8 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
         ykd.setItems(FXCollections.observableArrayList(Arrays.asList(s)));
         kdp.setItems(FXCollections.observableArrayList(Arrays.asList(s)));
         kva.setItems(FXCollections.observableArrayList(Arrays.asList(s)));
+        System.out.println("УПТ №" + number + " init");
+        Controller.uptList.add(this);
         P1.getSelectionModel().select(0);
         sh1.getSelectionModel().select(0);
         sh2.getSelectionModel().select(0);
@@ -80,13 +94,15 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
         ykd.getSelectionModel().select(0);
         kdp.getSelectionModel().select(0);
         kva.getSelectionModel().select(0);
-        System.out.println("УПТ №" + number + " init");
-        Controller.uptList.add(this);
-
+        reset = false;
+        counterCondown = 10;
+        startupCondown = 0;
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
 
-            if (getSelectedIndex(P1) > 1 || getSelectedIndex(P1) > 1 || (getSelectedIndex(sh1) > 1 && getSelectedIndex(sh1) < 3) ||
-                    (getSelectedIndex(sh2) > 1 && getSelectedIndex(sh2) < 3) || (getSelectedIndex(sh3) > 1 && getSelectedIndex(sh3) < 3) ||
+
+
+            if (getSelectedIndex(P1) > 1 || getSelectedIndex(P1) > 1 || (getSelectedIndex(sh1) > 1 && getSelectedIndex(sh1) < 4) ||
+                    (getSelectedIndex(sh2) > 1 && getSelectedIndex(sh2) < 4) || (getSelectedIndex(sh3) > 1 && getSelectedIndex(sh3) < 4) ||
                     getSelectedIndex(sdu) > 1 || getSelectedIndex(vu) > 1 || getSelectedIndex(kva) > 1 || getSelectedIndex(ykd) > 1
                     || getSelectedIndex(kdp) > 1 || getSelectedIndex(ob1) > 1 || getSelectedIndex(ao) > 1 || getSelectedIndex(gn) > 1 || getSelectedIndex(gu) > 1) {
                 error.setSelected(true);
@@ -95,21 +111,108 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
             } else {
                 error.setSelected(false);
             }
+            if (!upt_fire2.isSelected()) {
+                if ((getSelectedIndex(sh1) == 4 && getSelectedIndex(sh2) != 4 && getSelectedIndex(sh3) != 4) ||
+                        (getSelectedIndex(sh1) != 4 && getSelectedIndex(sh2) == 4 && getSelectedIndex(sh3) != 4) ||
+                        (getSelectedIndex(sh1) != 4 && getSelectedIndex(sh2) != 4 && getSelectedIndex(sh3) == 4)) {
+                    upt_fire1.setSelected(true);
+                } else {
+                    upt_fire1.setSelected(false);
+                }
 
-            if ((getSelectedIndex(sh1) == 4 && getSelectedIndex(sh2) != 4 && getSelectedIndex(sh3) != 4) ||
-                    (getSelectedIndex(sh1) != 4 && getSelectedIndex(sh2) == 4 && getSelectedIndex(sh3) != 4) ||
-                    (getSelectedIndex(sh1) != 4 && getSelectedIndex(sh2) != 4 && getSelectedIndex(sh3) == 4)) {
-                upt_fire1.setSelected(true);
-            } else {
-                upt_fire1.setSelected(false);
+                if ((getSelectedIndex(sh1) == 4 && getSelectedIndex(sh2) == 4) || (getSelectedIndex(sh1) == 4 && getSelectedIndex(sh3) == 4) ||
+                        (getSelectedIndex(sh2) == 4 && getSelectedIndex(sh3) == 4) || getSelectedIndex(sh1) == 5 || getSelectedIndex(sh2) == 5 ||
+                        getSelectedIndex(sh3) == 5) {
+                    upt_fire2.setSelected(true);
+                    //========================================================//
+                    //FIRE2 firefigtheng go/////////////////////////////////////
+                    //********************************************************//
+                    fire_from2.setSelected(true);
+                    fire_from1.setSelected(true);
+
+                    sh1.setDisable(true);
+                    sh2.setDisable(true);
+                    sh3.setDisable(true);
+                    сondown.setText(String.format("%d", counterCondown));
+
+                } else {
+                    upt_fire2.setSelected(false);
+                }
+            }
+            if (getSelectedIndex(kdp) == 1) {
+                avt_off.setSelected(true);
+                upt_fire2.setSelected(true);
+                fire_from2.setSelected(true);
+                fire_from1.setSelected(false);
+            }
+            if (getSelectedIndex(kva) == 1) {
+                avt_off.setSelected(false);
             }
 
-            if ((getSelectedIndex(sh1) == 4 && getSelectedIndex(sh2) == 4) || (getSelectedIndex(sh1) == 4 && getSelectedIndex(sh3) == 4) ||
-                    (getSelectedIndex(sh2) == 4 && getSelectedIndex(sh3) == 4) || getSelectedIndex(sh1) == 5 || getSelectedIndex(sh2) == 5 ||
-                    getSelectedIndex(sh3) == 5) {
-                upt_fire2.setSelected(true);
-            } else {
+            if (getSelectedIndex(ykd) == 1) {
+                avt_off.setSelected(true);
+            }
+            if (getSelectedIndex(sdu) == 1) {
+                otv_derived.setSelected(true);
+            }
+            if (reset) {
+                P1.getSelectionModel().select(0);
+                sh1.getSelectionModel().select(0);
+                sh2.getSelectionModel().select(0);
+                sh3.getSelectionModel().select(0);
+                sdu.getSelectionModel().select(0);
+                vu.getSelectionModel().select(0);
+                gu.getSelectionModel().select(0);
+                gn.getSelectionModel().select(0);
+                ao.getSelectionModel().select(0);
+                ob1.getSelectionModel().select(0);
+                ykd.getSelectionModel().select(0);
+                kdp.getSelectionModel().select(0);
+                kva.getSelectionModel().select(0);
+                sh1.setDisable(false);
+                sh2.setDisable(false);
+                sh3.setDisable(false);
+                reset = false;
+                counterCondown = 10;
+                startupCondown = 0;
+                otv_derived.setSelected(false);
                 upt_fire2.setSelected(false);
+                upt_fire1.setSelected(false);
+                stop_startup.setSelected(false);
+                startup_is_goin.setSelected(false);
+                otv_derived.setSelected(false);
+                fire_from2.setSelected(false);
+                fire_from1.setSelected(false);
+            }
+
+            if (!upt_fire2.isSelected()) {
+                stop_startup.setSelected(false);
+            }
+//fire algoritm//
+            if (upt_fire2.isSelected()) {
+                if (stop_startup.isSelected()) {
+                    сondown.setText("0");
+                    counterCondown = 10;
+                } else {
+                    сondown.setText(String.format("%d", counterCondown));
+                    if (counterCondown > 0) {
+                        counterCondown--;
+                    } else {
+                        if (startupCondown < 5) {
+                            P1.getSelectionModel().select(1);
+                        } else {
+                            startup_is_goin.setSelected(true);
+                            startupCondown = 5;
+                            P1.getSelectionModel().select(0);
+                            if (otv_derived.isSelected()) {
+                                //ОТВ подано
+                            } else {
+                                //ОТВ не подано
+                            }
+                        }
+                        startupCondown++;
+                    }
+                }
             }
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
@@ -122,18 +225,36 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
 
     public byte[] getState() {
         state = new byte[]{1, 4, 10,
-                0x01,
+                getByteStateZero_upt(),
                 getByteState_upt(),
                 getState_sh1_sh2(),
                 getState_sh3_P1(),
                 getTablo(),
                 getUKD(),
-                (byte) (Double.parseDouble(сondown.getText()) * 10 - 100),
+                Byte.parseByte(сondown.getText()),
                 (byte) (Double.parseDouble(u_power.getText()) * 10 - 100),
                 (byte) (Double.parseDouble(u_power_akb.getText()) * 10 - 100),
                 0x00};
         return state;
     }
+
+    private byte getByteStateZero_upt() {
+        byte b = 0x01;
+        //todo RIP state
+        if (fire_from2.isSelected())
+            b |= 0x04;
+        if (fire_from1.isSelected())
+            b |= 0x08;
+        if (stop_startup.isSelected())
+            b |= 0x10;
+        if (otv_derived.isSelected())
+            b |= 0x20;
+        if (startup_is_goin.isSelected())
+            b |= 0x40;
+
+        return b;
+    }
+
 
     private byte getByteState_upt() {
         byte b = 0x00;
@@ -143,8 +264,8 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
             b |= 0x02;
         if (error_akb.isSelected())
             b |= 0x04;
-        if (door_open.isSelected())
-            b |= 0x08;
+//        if (stop_startup.isSelected())//duplicated
+//            b |= 0x08;
         if (upt_is_blocked.isSelected())
             b |= 0x10;
         if (upt_fire1.isSelected())
@@ -196,6 +317,7 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
                     switch (input.get(2)) {
                         case 0x01:
                             System.out.println("RESET");
+                            reset = true;
                             ((SerialPortUpt) o).response(new byte[]{number, 0x06, 0x01});
                             break;
                         case 0x02:
@@ -206,6 +328,11 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
                         case 0x03:
                             System.out.println("STARTUP");
                             ((SerialPortUpt) o).response(new byte[]{number, 0x06, 0x03});
+                            upt_fire2.setSelected(true);
+                            stop_startup.setSelected(false);
+                            avt_off.setSelected(true);
+                            fire_from1.setSelected(true);
+                            fire_from2.setSelected(true);
                             break;
                         case 0x04:
                             System.out.println("Look");
@@ -218,6 +345,26 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
                         case 0x05:
                             System.out.println("test display");
                             ((SerialPortUpt) o).response(new byte[]{number, 0x06, 0x05});
+                            break;
+                        case 0x21:
+                            System.out.println("Start STARTUP no timeout");
+                            ((SerialPortUpt) o).response(new byte[]{number, 0x06, 0x21});
+                            if (upt_fire2.isSelected()) {
+                                stop_startup.setSelected(false);
+                                counterCondown = 0;
+                            }
+                            break;
+                        case 0x20:
+                            System.out.println("STOP STARTUP ");
+                            ((SerialPortUpt) o).response(new byte[]{number, 0x06, 0x20});
+                            if (upt_fire2.isSelected()) {
+                                stop_startup.setSelected(true);
+                            }
+                            break;
+                        default:
+                            System.out.println("RESET");
+                            reset = true;
+                            ((SerialPortUpt) o).response(new byte[]{number, 0x06, 0x01});
                             break;
 
                     }
@@ -239,5 +386,9 @@ public class Upt implements Initializable, Observer, EventHandler<WindowEvent> {
     @Override
     public void handle(WindowEvent event) {
 
+    }
+
+    public void resetDevice(ActionEvent actionEvent) {
+        reset = true;
     }
 }
